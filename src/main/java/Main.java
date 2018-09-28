d74d266d6b254a30fd86b09cffad66512a52b893
Ciprian Nicuta <ciprian.nicuta@endava.com>
2018-09-28 10:42:25 +0300

    initial text in xBug added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Initial text in master");
+        System.out.println("Initial text in xBug");
     }
 }
